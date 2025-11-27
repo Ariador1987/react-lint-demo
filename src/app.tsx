f436dd32df9536d8ea5843fe7ef0c68f985b1642
Ariador1987 <bozidar.bralic@gmail.com>
2025-11-27 08:25:04 +0100

    testing precommit hooks

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 import reactLogo from "./assets/react.svg";
 
-import viteLogo from "/vite.svg"
+import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
@@ -32,15 +32,25 @@ function App() {
 						className="logo react"
 						alt="React logo"
 					/>
+					<h2>TESTING</h2>
 				</a>
 			</div>
 			<h1>Vite + React</h1>
 			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
+				<button
+					type="button"
+					onClick={() => setCount(count => count + 1)}
+				>
+					count is
+					{" "}
+					{count}
 				</button>
 				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
+					Edit
+					{" "}
+					<code>src/App.tsx</code>
+					{" "}
+					and save to test HMR
 				</p>
 			</div>
 			<p className="read-the-docs">
